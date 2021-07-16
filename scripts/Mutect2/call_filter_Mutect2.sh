c04a8860d2d42d1cdeae49274b55061b2e95e399
amunzur <admunzur.21@gmail.com>
2021-07-16 10:12:49 -0700

    started updating such that the script can take a text file with file names as an input, will continue

--- scripts/Mutect2/call_filter_Mutect2.sh
+++ scripts/Mutect2/call_filter_Mutect2.sh
@@ -5,7 +5,7 @@ source ${configfile};
 source ${conda_profile_path};
 conda activate mito 
 
-type=$1
+type="WBC"
 
 if [[ "$type" == "ctDNA" ]]
 then
@@ -16,8 +16,16 @@ else
 		printf "WBC DNA\n"
 fi
 
+###########################################
+# option 1: run the code on all vcf files in the dir given below
+###########################################
 for vcf_file in GU-*.bam_vcf 
 do
 	printf "bash ${script_dir}/filter_Mutect2.sh ${vcf_file} ${type} "
 	sbatch --exclude=cn[01-05] /groups/wyattgrp/users/amunzur/chip_project/scripts/Mutect2/filter_Mutect2.sh ${vcf_file} ${type};
 done
+
+###########################################
+# option 2: run the code on some samples only
+# This option uses the same list of samples as the run_Mutect2.sh script.
+###########################################
