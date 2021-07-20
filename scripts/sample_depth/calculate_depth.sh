bc786abba5c279f29cd843a187003b08f4681a74
amunzur <admunzur.21@gmail.com>
2021-07-20 10:06:38 -0700

    update

--- scripts/sample_depth/calculate_depth.sh
+++ scripts/sample_depth/calculate_depth.sh
@@ -3,15 +3,16 @@
 # run this in the dir with the bams of interest:
 # cd /groups/wyattgrp/users/amunzur/chip_project/finland_bams/ctDNA_prognosis
 # ls -d -1 "$PWD/"filtered_RG_*.bam | parallel -j10 -k bash /groups/wyattgrp/users/amunzur/chip_project/scripts/sample_depth/calculate_depth.sh
-
+# find $PWD -name '*.bam' -a ! -name '*filtered*' | parallel -j10 -k bash /groups/wyattgrp/users/amunzur/chip_project/scripts/sample_depth/calculate_depth.sh
 # ca variant_calling
+
 bam=$1 # supplied by GNU parallel in the pipe
 path_to_bed="/groups/wyattgrp/users/amunzur/chip_project/references/baits_hg38.bed" 
 dir_to_read_depth="/groups/wyattgrp/users/amunzur/chip_project/metrics/coverage_information/" # the path where read depth information will be saved, OUTPUT
 # output_path="/groups/wyattgrp/users/amunzur/chip_project/metrics/coverage_information/GU_finland_download_coverage.txt"
 
-output_file_txt="${dir_to_read_depth}new_finland_download_FILTERED.txt"
-output_file_csv="${dir_to_read_depth}new_finland_download_FILTERED.csv"
+output_file_txt="${dir_to_read_depth}new_finland_download_RAW.txt"
+output_file_csv="${dir_to_read_depth}new_finland_download_RAW.csv"
 
 touch $output_file_txt # initialize an empty file to append coverage information
 
