984ec15c115576d1adf14e4f7e54ed64d2be2de7
amunzur <admunzur.21@gmail.com>
2021-08-03 15:30:11 -0700

    =Given a csv file of variants, this script subsets the called variants to the locations in the gene panel, and save a new copy of the file.

---
+++ scripts/Rscripts/subset_to_panel.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+
+path_to_bed <- "/groups/wyattgrp/users/amunzur/chip_project/references/baits_hg38.bed" # bed file with locations of interest
+
+path_to_df <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/ALL_MERGED.csv" # df to be subsetted
+path_to_df_subsetted <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/ALL_MERGED_panel.csv"
+
+path_to_df <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/ALL_MERGED_FILTERED_0.2.csv"
+path_to_df_subsetted <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/ALL_MERGED_FILTERED_0.2_panel.csv"
+
+path_to_df <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/ALL_MERGED_FILTERED_INDEL.csv"
+path_to_df_subsetted <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/ALL_MERGED_FILTERED_INDEL_panel.csv"
+
+path_to_df <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/curated_muts.csv"
+path_to_df_subsetted <- "/groups/wyattgrp/users/amunzur/chip_project/subsetted/new_finland_download/curated_muts_panel.csv"
+
+subset_to_panel <- function(path_to_bed, path_to_df, path_to_df_subsetted) {
+	bed <- as.data.frame(read.delim(path_to_bed, header = FALSE))
+	names(bed) <- c("chrom", "start", "stop", "gene")
+	df <- as.data.frame(read_csv(path_to_df))
+
+	to_keep <- list() # list of positions to remove
+
+	i <- 1 
+	print(i)
+	while (i <= dim(df)[1]){
+		chrom_subsetted <- df[i, 1] # pick the chrom we are at 
+		location <- df[i, 2] # pick the location we are at 
+		bed_subsetted <- bed %>% filter(chrom == chrom_subsetted) # subset the bed by chrom
+		message(c("position:", i))
+
+		j <- 1
+		while(j <= dim(bed_subsetted)[1]) {
+			start_pos <- bed_subsetted[j, 2]
+			end_pos <- bed_subsetted[j, 3]
+
+			if (all(location >= start_pos, location <= end_pos)) {
+				to_keep <- append(to_keep, i) # saving the row index of muts we are keeping
+				break 
+				print("Position in panel.")
+				} else {j <- j + 1} # if the location isn't in the panel, go check out the next position in the bed file.
+				# print(c(j, "j"))
+
+		} # end of inner while loop
+
+	i <- i + 1 # next identified variant
+
+	} # end of outer while loop - looping through identified variants
+
+	df <- df[unlist(to_keep), ]
+	write_csv(df, path_to_df_subsetted)
+
+	return(df)
+
+} # end of function
+
+subset_to_panel(path_to_bed, path_to_df, path_to_df_subsetted)
+
+
